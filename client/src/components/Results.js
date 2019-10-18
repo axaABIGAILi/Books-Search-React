abf951dfcf3e91894b4dab4346c616c415758b04
Abi D <axa.abigail.i@gmail.com>
2019-10-17 23:45:38 -0500

    combined Searchbar component with Results component

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -5,6 +5,23 @@ const Results = props => {
     <div className="container">
       <div className="row">
         <div className="col">
+            <div className="card mb-3">
+                <div className="card-body">
+                  <div className="row">
+                  <label for="searchInput" className="col-sm-2 col-form-label">Search</label>
+                  <div className="col-sm-9">
+                    <input id="searchInput" className="form-control" type="search" placeholder="Find a book by title"></input>
+                  </div>
+                  <div class="col-sm-1">
+                    <button id="searchbtn" class="btn bg-light" type="submit" onClick={props.handleBookSearch}><i class="fa fa-search"></i></button>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
                 <div className="card mb-3">
                     <div className="row no-gutters">
                       <div className="col-md-4">
